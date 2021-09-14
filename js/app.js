40d931a1418682e4ed57481fbb0c35e1e106e705
Tasleemofx <oladepotesleem5@gmail.com>
2021-09-14 09:56:49 +0100

    first commit of portfolio

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector('#burger');
+const cross = document.querySelector('#cross')
+const showlist = document.querySelector('nav ul');
+const bar1 = document.querySelector('.d1');
+const bar2 = document.querySelector('.d2');
+const bar3 = document.querySelector('.d3')
+burger.addEventListener("click",()=>{
+    showlist.classList.add('show-300')
+})
+cross.addEventListener('click',()=>{
+    showlist.classList.remove('show-300')
+})
+const navUl = document.querySelectorAll('nav ul li');
+navUl.forEach((listItem)=>{
+    listItem.addEventListener('click',()=>{
+        showlist.classList.remove('show-300')
+    })
+})
